e59ae662f202cb65a396c5c7f82c57264d51e1b9
kurumi <krkrteikoku@gmail.comi>
2020-06-18 22:16:08 +0900

    ツイート編集機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,17 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "ツイートを編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   def task_params
     params.require(:task).permit(:content)
