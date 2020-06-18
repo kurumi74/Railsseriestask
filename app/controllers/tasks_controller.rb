5becf75e4e5af17f44f00524462b5094d9c6d755
kurumi <krkrteikoku@gmail.comi>
2020-06-18 18:59:07 +0900

    タイムラインの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,16 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
   def new
+    @task = Task.new
+  end
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+  private
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
